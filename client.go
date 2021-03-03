cf3e384740757b40cbb122c4a5c4463ef222fd42
Suraj Jain <imsuraj.sj@gmail.com>
2021-03-03 22:34:50 +0530

    Add client

---
+++ client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/yaitsmesj/gRPC-to-REST/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	client := pb.NewUserServiceClient(conn)
+	user, err := client.GetUser(context.Background(), &pb.GetUserRequest{Id: 2})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(user)
+
+	list, err := client.GetUserList(context.Background(), &pb.GetUserListRequest{Page: 2})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(list)
+
+	nuser, err := client.Create(context.Background(), &pb.CreateRequest{Name: "Suraj", Job: "Engineer"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(nuser)
+
+	muser, err := client.Update(context.Background(), &pb.UpdateRequest{Name: "jj", Job: "Engineer", Id: 2})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(muser)
+
+	duser, err := client.Delete(context.Background(), &pb.DeleteRequest{Id: 2})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(duser)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yaitsmesj/gRPC-to-REST-client
+
+go 1.15
+
+require (
+	github.com/yaitsmesj/gRPC-to-REST v0.0.0-20210303165356-f3722d9476db // indirect
+	google.golang.org/grpc v1.36.0 // indirect
+)
